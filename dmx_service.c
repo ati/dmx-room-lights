df43109b2f1af56aef285f20776211b1c4849ec3
Alexander <variomap@gmail.com>
2012-04-10 16:11:30 +0400

    dmx_service starts

--- dmx_service.c
+++ dmx_service.c
@@ -10,6 +10,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <errno.h>
 #include <ftdi.h>
 
 
@@ -19,18 +20,21 @@
 #define DMX_CHANNELS 512
 #define BAUD_RATE 250000
 
-#define UDP_PORT 57211
+#define UDP_PORT 37211
 
 
 struct ftdi_context ftdic;
 
-// typedef unsigned char dmx_t;
+//typedef unsigned char dmx_t;
+//dmx_t dmx[DMX_CHANNELS + 1];
 
-typedef struct dmx_t {
+#pragma pack(1)
+struct dmx_t {
     unsigned char head;
     unsigned char body[DMX_CHANNELS];
-} dmx;
+};
 
+struct dmx_t dmx;
 
 static int dmx_init(struct ftdi_context* ftdic)
 {
@@ -71,7 +75,7 @@ static int dmx_init(struct ftdi_context* ftdic)
 
 
 static int do_dmx_break(struct ftdi_context* ftdic)
-{]
+{
     int ret;
     
     if ((ret = ftdi_set_line_property2(ftdic, BITS_8, STOP_BIT_2, NONE, BREAK_ON)) < 0)
@@ -90,13 +94,14 @@ static int do_dmx_break(struct ftdi_context* ftdic)
 }
 
 
-static int dmx_write(struct ftdi_context* ftdic, dmx_t* dmx, size_t size)
+static int dmx_write(struct ftdi_context* ftdic, struct dmx_t dmx, size_t size)
 {
     int ret;
 
     if ((ret = do_dmx_break(ftdic)) == EXIT_SUCCESS)
     {
-        if ((ret = ftdi_write_data(ftdic, dmx, size)) < 0)
+        unsigned char *dmx_packet = (unsigned char *)&dmx;
+        if ((ret = ftdi_write_data(ftdic, dmx_packet, size)) < 0)
         {
             fprintf(stderr, "unable to write data: %d (%s)\n", ret, ftdi_get_error_string(ftdic));
             ret = EXIT_FAILURE;
@@ -106,11 +111,11 @@ static int dmx_write(struct ftdi_context* ftdic, dmx_t* dmx, size_t size)
 }
 
 
-static int universe_set(dmx_t* dmx, size_t size)
+static int universe_set(struct dmx_t dmx, size_t size)
 {
     int ret = EXIT_FAILURE;
 
-    if ((do_dmx_break(&ftdic) == EXIT_SUCCESS) && (dmx_write(&ftdic, dmx, size)) == EXIT_SUCCESS))
+    if ((do_dmx_break(&ftdic) == EXIT_SUCCESS) && (dmx_write(&ftdic, dmx, size) == EXIT_SUCCESS))
     {
         ret = EXIT_SUCCESS;
     }
@@ -139,14 +144,14 @@ int main(int argc, char **argv)
     }
 
     // initialize universe
-    memset(&dmx[0], 0, sizeof(dmx));
+    memset(&dmx, 0, sizeof(dmx));
 
     // initialize UDP server
     int sockfd, n;
     struct sockaddr_in servaddr, cliaddr;
     socklen_t len;
 
-    if (sockfd = socket(AF_INET,SOCK_DGRAM,0) < 0)
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         fprintf(stderr, "error %d creating socket: %s\n", errno, strerror(errno));
         dmx_shutdown();
@@ -165,10 +170,12 @@ int main(int argc, char **argv)
         return EXIT_FAILURE;
     }
 
+    fprintf(stdout, "initialized DMX hardware and network service. Listening to UDP connections at port %d\n", UDP_PORT);
+
     for (;;)
     {
       len = sizeof(cliaddr);
-      n = recvfrom(sockfd, dmx.body, sizeof(message), 0, (struct sockaddr *)&cliaddr, &len);
+      n = recvfrom(sockfd, dmx.body, sizeof(dmx.body), 0, (struct sockaddr *)&cliaddr, &len);
       fprintf(stdout, "Received %d bytes:\n", n);
       universe_set(dmx, sizeof(dmx));
       usleep(4*DMX_CHANNELS);
