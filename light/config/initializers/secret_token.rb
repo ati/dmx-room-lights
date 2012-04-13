7c134fa3ae8b4348e49040ae45fc97afe38813c1
Alexander <ati@undev.ru>
2012-04-13 20:18:49 +0400

    empty rails project

---
+++ light/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Light::Application.config.secret_token = '58ef689406e05c32ee2e3a713bf08bf2e865c79f86fd8f3f7659e6268a5f682df2ca2723ebfa4288a4ff80cdb63a7e796a14d737c8cc78a05619c8e161bb1271'
---
+++ light/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Light::Application.config.session_store :cookie_store, :key => '_light_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Light::Application.config.session_store :active_record_store
