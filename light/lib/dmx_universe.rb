c75f764b7110c50fe0218815c7d66beb3ca2ca4b
Alexander <ati@undev.ru>
2012-04-24 13:54:26 +0400

    fixet typos and hsv2rbg color conversion

--- light/lib/dmx_universe.rb
+++ light/lib/dmx_universe.rb
@@ -1,5 +1,12 @@
 class DmxUniverse
-  def display(bytes)
+
+  DMX_HOST = 'localhost'
+  DMX_PORT = 37211
+
+  def self.display(bytes)
+    #s = UDPSocket.new
+    #s.send(bytes.pack("C*"), 0, DMX_HOST, DMX_PORT)
+    puts bytes.inspect
     return true
   end
 end
